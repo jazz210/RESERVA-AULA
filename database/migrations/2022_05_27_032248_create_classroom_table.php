43778e60d3342938378cbab8a657dee6f33cf272
Karen Paco <p.karen1093@gmail.com>
2022-05-26 23:54:46 -0400

    agregado la tabla y el modelo de aulas

---
+++ database/migrations/2022_05_27_032248_create_classroom_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name_classroom');
+            $table->string('ubicacion');
+            $table->unsignedBigInteger('reservation_id');
+            $table->foreign('reservation_id')->references('id')->on('reservations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classroom');
+    }
+}

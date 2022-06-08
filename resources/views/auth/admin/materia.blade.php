95a4ed00de0d4971a9e80975d181d89517b37ef4
jazz210 <carlos.uscamayta@metrixx.net>
2022-06-08 07:40:54 -0400

    materia

---
+++ resources/views/auth/admin/materia.blade.php
@@ -0,0 +1,44 @@
+@extends('auth.admin.admin')
+@section('title','register')
+@section('content')
+
+<div class="block mx-auto my-12 p-8   w-1/3 border-gray-400 rounded-lg shadow-lg" style="background-color:rgba(50, 156, 192, 0.5)">
+
+    <h1 class="text-3xl text-center fond-bold ">Añadir materia</h1>
+    <form class="mt-4" method="POST" action="">
+        @csrf
+
+        <input type="name" class="border border-gray-200 rounded-md bg-gray-200 w-full
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Nombre Materia" id="name" name="name">
+        @error('name')
+      <p class="border border-red-500 rounded-md bg-red-100 w-full
+      text-red-600 p-2 my-2">* {{ $message }}</p>
+    @enderror
+
+        <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Codigo materia" id="email" name="email">
+        @error('email')
+      <p class="border border-red-500 rounded-md bg-red-100 w-full
+      text-red-600 p-2 my-2">* {{ $message }}</p>
+    @enderror
+
+        <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full
+        text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Contraseña" id="password" name="password">
+        @error('password')
+      <p class="border border-red-500 rounded-md bg-red-100 w-full
+      text-red-600 p-2 my-2">* {{ $message }}</p>
+    @enderror
+
+       <input type="password" class="border border-gray-200 rounded-md bg-gray-200
+    w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
+    placeholder="Password confirmation" id="password_confirmation"
+    name="password_confirmation">
+
+
+        <button type="submit" class="rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 my-3 hover:bg-indigo-600">Crear Docente</button>
+        <button type="button" class="btn btn-success rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 my-3 hover:bg-indigo-600"name="Cancelar" value="Cancelar" onclick="location.href='/admin'">Salir </button>
+    </form>
+
+</div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
     <!-- Tailwind CSS Link -->
     <link rel="stylesheet"
     href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css">
+    
   </head>
+  
 
   <body class=" text-gray-800">
      
@@ -38,25 +40,103 @@
         <a class="mx-8">
           <p class="text-xl">Bienvenido <b>{{ auth()->user()->name }}</b></p>
         </a>
+        
       <form action="login" method="post" class="navbar-brand">
             @method('put')
             @csrf
             <button class="btn font-semibold
           border-2 border-white py-2 px-4 rounded-md hover:bg-red-500
           hover:text-indigo-700">Cerrar_Sesion</button>
+          
         </form>
-      @else
-        <li>
-          <a href="{{ route('login.index') }}" class="font-semibold
-          border-2 border-white py-2 px-4 rounded-md hover:bg-white
-          hover:text-indigo-700">Log_In</a>
-        </li>
-        <li>
-          <a href="{{ route('aulas.index') }}" class="font-semibold
-          border-2 border-white py-2 px-4 rounded-md hover:bg-white
-          hover:text-indigo-700">Aulas</a>
-        </li>
+      </nav>
+      <header>
+        <nav class="bg-gray-800">
+          <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="relative flex items-center justify-between h-16">
+              <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <!-- Mobile menu button-->
+                <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <span class="sr-only">Open main menu</span>
+                  <!--
+                    Icon when menu is closed.
         
+                    Heroicon name: outline/menu
+        
+                    Menu open: "hidden", Menu closed: "block"
+                  -->
+                  <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
+                  <!--
+                    Icon when menu is open.
+        
+                    Heroicon name: outline/x
+        
+                    Menu open: "block", Menu closed: "hidden"
+                  -->
+                  <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div class="hidden sm:block sm:ml-0">
+                  <div class="flex space-x-8">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <a href="{{ route('gestionar.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Gestionar</a>
+        
+                    <a href="{{ route('informacionreservas.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Informacion Reservas</a>
+        
+                    <a href="{{ route('gestionaraulas.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Gestionar Aulas</a>
+        
+                    <a href="{{ route('register.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Registrar</a>
+      
+                    <a href="{{ route('listareservas.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium aria-current="page">Lista Reservas</a>
+        
+                    <a href="{{ route('solicitudes.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Solicitudes</a>
+        
+                    <a href="{{ route('asignaraula.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Asignar Aula</a>
+        
+                    <a href="{{ route('solicitudesaceptadas.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Solicitudes Aceptadas</a>
+
+                    <a href="{{ route('materia.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Añadir materia</a>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        
+          
+          <div class="sm:hidden" id="mobile-menu">
+            <div class="px-2 pt-2 pb-3 space-y-1">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a href="{{ route('gestionar.index') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Gestionar</a>
+        
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Informacion Reservas</a>
+        
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Gestionar Aulas</a>
+        
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Registrar</a>
+      
+              <a href="{{ route('listareservas.index') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Lista Reservas</a>
+        
+              <a href="{{ route('solicitudes.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Solicitudes</a>
+        
+              <a href="{{ route('asignaraula.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Asignar Aula</a>
+        
+              <a href="{{ route('solicitudesaceptadas.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base nt-medium">Solicitudes</a>
+fo
+              <a href="{{ route('materia.index') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Añadir materia</a>
+            </div>
+          </div>
+        </nav>
+       
+      </header>
+    </ul>
+      @else
+      
       @endif
 
       </ul>
@@ -118,9 +198,8 @@ margin: auto;
 </style>
     @yield('content')
     
-
     
  </body>
-
+ 
 
 </html>
